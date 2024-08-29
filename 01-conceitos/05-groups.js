33f740dd01c7e97da02075b2901fd9ced1b75fc0
leonardopadilha <leonardopadilha28@gmail.com>
2024-08-29 05:20:38 -0300

    Conceito sobre grupos no k6

---
+++ 01-conceitos/05-groups.js
@@ -0,0 +1,31 @@
+import { check, group } from 'k6'
+import http from 'k6/http'
+
+export const options = {
+    vus: 4,
+    durations: '5s',
+    thresholds: {
+        'http_req_duration{group:::requisição por id}': ['p(95) < 500']
+    }
+}
+
+export default function() {
+    group('requisição todos os crocodilos', function() {
+        const response1 = http.get('https://test-api.k6.io/public/crocodiles')
+        check(response1, {
+            'status code 200 get all': (r) => r.status === 200
+        })
+    })
+
+    group('requisição por id', function() {
+        const response2 = http.get('https://test-api.k6.io/public/crocodiles/1')
+        check(response2, {
+            'status code 200 get id': (r) => r.status === 200
+        })
+    })
+}
+
+/*
+k6 run .\05-groups.js => Executa o arquivo de teste
+k6 run -o csv=resultado.csv .\05-groups.js => Executa o arquivo de teste e gera o csv no final
+*/
